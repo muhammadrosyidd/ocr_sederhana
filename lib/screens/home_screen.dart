e759b8bb3c689a7817a8906e68d5cffce05c1291
muhammadrosyidd <muhammadrosyidd>
2025-10-23 13:42:49 +0700

    UTS: Selesai Soal 1 - ListTile dan Navigasi Balik

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -3,20 +3,27 @@ import 'scan_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Menu Utama')),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (_) => const ScanScreen()),
-            );
-          },
-          child: const Text('Mulai Scan Teks'),
-        ),
+      body: ListView(
+        children: [
+          ListTile(
+            leading: const Icon(Icons.camera_alt, color: Colors.blue),
+            title: const Text('Mulai Pindai Teks Baru'),
+            subtitle: const Text('Gunakan kamera untuk memindai teks dari gambar'),
+            trailing: const Icon(Icons.arrow_forward_ios, size: 16),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (_) => const ScanScreen()),
+              );
+            },
+          ),
+          const Divider(height: 1),
+        ],
       ),
     );
   }
